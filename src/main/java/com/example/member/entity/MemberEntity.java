02102205d8197f0b965638b033e5b9db1f8a7290
sooa1204 <jungswa1204@gmail.com>
2024-01-22 19:16:45 +0900

    feat: Spring Boot를 활용한 회원가입 기능 추가 Thymeleaf 템플릿 엔진을 사용하여 폼을 제공하고, Ajax를 이용하여 이메일 중복 체크를 비동기적으로 처리.

---
+++ src/main/java/com/example/member/entity/MemberEntity.java
@@ -0,0 +1,46 @@
+package com.example.member.entity;
+
+import com.example.member.dto.MemberDTO;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+
+@Entity
+@Setter
+@Getter
+@Table(name = "member_table")
+public class MemberEntity {
+    @Id // pk 지정
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // auto_increment
+    private Long id;
+
+    @Column(unique = true) // unique 제약조건 추가
+    private String memberEmail;
+
+    @Column
+    private String memberPassword;
+
+    @Column
+    private String memberName;
+
+    public static MemberEntity toMemberEntity(MemberDTO memberDTO) {
+        MemberEntity memberEntity = new MemberEntity();
+        memberEntity.setMemberEmail(memberDTO.getMemberEmail());
+        memberEntity.setMemberName(memberDTO.getMemberName());
+        memberEntity.setMemberPassword(memberDTO.getMemberPassword());
+
+        return memberEntity;
+    }
+
+    public static MemberEntity toUpdateMemberEntity(MemberDTO memberDTO) {
+        MemberEntity memberEntity = new MemberEntity();
+        memberEntity.setId(memberDTO.getId());
+        memberEntity.setMemberEmail(memberDTO.getMemberEmail());
+        memberEntity.setMemberName(memberDTO.getMemberName());
+        memberEntity.setMemberPassword(memberDTO.getMemberPassword());
+
+        return memberEntity;
+    }
+}
